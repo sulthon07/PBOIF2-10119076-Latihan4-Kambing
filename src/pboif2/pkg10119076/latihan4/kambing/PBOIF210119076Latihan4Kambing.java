c826ee7fa027f92f440528c99f12cf8cddf4eda4
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 17:23:08 +0700

    PBOIF2-10119076-Latihan4-Kambing

---
+++ src/pboif2/pkg10119076/latihan4/kambing/PBOIF210119076Latihan4Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan4.kambing;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : Kambing
+ */
+public class PBOIF210119076Latihan4Kambing {
+
+    public void tambahKambing() {
+        //Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("julmah kambing setelah ditambah: " + jumlahKambing);
+         
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOIF210119076Latihan4Kambing kambingJantan = new PBOIF210119076Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
